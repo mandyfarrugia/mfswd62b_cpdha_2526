85efa9618ca2e52df132d7ba1f106488facae396
mandyfarrugia <mandy.farrugia.c10352@mcast.edu.mt>
2026-01-21 11:59:02 +0100

    Designing a form to add a new asset.

--- mfswd62b_cpd/lib/widgets/asset_management_add_screen.dart
+++ mfswd62b_cpd/lib/widgets/asset_management_add_screen.dart
@@ -1,8 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:mfswd62b_cpd/widgets/asset_card_button.dart';
 import 'package:mfswd62b_cpd/widgets/asset_management_navigation_menu_drawer.dart';
 
-class AssetManagementAddScreen extends StatelessWidget {
+import '../models/asset_type.dart';
+
+class AssetManagementAddScreen extends StatefulWidget {
   const AssetManagementAddScreen({super.key});
+
+  @override
+  State<AssetManagementAddScreen> createState() {
+    return _AssetManagementAddScreenState();
+  }
+}
+
+class _AssetManagementAddScreenState extends State<AssetManagementAddScreen> {
+  final TextEditingController _assetNameController = TextEditingController();
+  final TextEditingController _serialNumberController = TextEditingController();
+  var _selectedAssetType = AssetType.adapter;
+
+  @override
+  void dispose() {
+    this._assetNameController.dispose();
+    this._serialNumberController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,25 +46,74 @@ class AssetManagementAddScreen extends StatelessWidget {
         padding: const EdgeInsets.all(16),
         child: Column(
           children: [
-            TextField(
-              keyboardType: TextInputType.text,
-              decoration: const InputDecoration(
-                label: Text('Asset Name')
-              ),
+            Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    keyboardType: TextInputType.text,
+                    controller: _assetNameController,
+                    decoration: const InputDecoration(
+                      labelText: 'Asset Name'
+                    ),
+                  )
+                )
+              ],
+            ),
+            const SizedBox(width: 12),
+            Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                  keyboardType: TextInputType.text,
+                  controller: _serialNumberController,
+                  decoration: const InputDecoration(
+                    labelText: 'Serial Number'
+                  ),
+                ))
+              ],
             ),
             const SizedBox(height: 12),
-            TextField(
-              keyboardType: TextInputType.text,
-              decoration: const InputDecoration(
-                label: Text('Serial Number')
-              ),
+            Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    keyboardType: TextInputType.text,
+                    decoration: const InputDecoration(
+                      labelText: 'Description'
+                    ),
+                  )
+                )
+              ]
             ),
             const SizedBox(height: 12),
-            TextField(
-              keyboardType: TextInputType.text,
-              decoration: const InputDecoration(
-                label: Text('Description')
-              ),
+            Row(
+              children: [
+                DropdownButton(
+                  value: this._selectedAssetType,
+                  items: AssetType.values.map((assetType) =>
+                    DropdownMenuItem(
+                      value: assetType,
+                      child: Text(assetType.name)
+                    )
+                  ).toList(),
+                  onChanged: (value) {
+                    if(value == null) return;
+                    setState(() {
+                      this._selectedAssetType = value;
+                    });
+                  },
+                )
+              ],
+            ),
+            const SizedBox(width: 12),
+            Row(children: [
+                AssetCardButton(
+                  backgroundColour: Colors.deepPurpleAccent,
+                  foregroundColour: Colors.white,
+                  buttonText: 'Add new asset',
+                  callback: () {}
+                )
+              ],
             )
           ],
         )
